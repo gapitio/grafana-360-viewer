57627c207f14254fb5fc20028574dc201cabd248
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-07-28 07:55:05 +0200

    WIP

--- src/dev-site/html-node.ts
+++ src/dev-site/html-node.ts
@@ -4,10 +4,4 @@ window.htmlNode = document
 
 htmlNode.onpanelupdate = () => null;
 
-htmlNode.innerHTML = `<style>@import "./build/bundle.css"</style>
-<div
-  id="panorama-container"
-  style="position: absolute; top: 0; left: 0; width: 100%; height: 100%"
->
-  <div id="hotspot">Drag me</div>
-</div>`;
+htmlNode.innerHTML = `<style>@import "./build/bundle.css"</style><div></div>`;
