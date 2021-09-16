0ea190ee7f2231c73d3718643a732b219a915a67
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-09-16 13:12:42 +0200

    feat: add hotspot tab

---
+++ src/components/List/HotspotList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { hotspotConfigListStore } from "../../stores";
+  import HotspotItem from "./HotspotItem.svelte";
+</script>
+
+<div>
+  <ul>
+    {#each $hotspotConfigListStore as hotspotConfig}
+      <HotspotItem {hotspotConfig} />
+    {/each}
+  </ul>
+</div>
+
+<style>
+  div {
+    color: #fcfcfc;
+    overflow-y: auto;
+    flex: 1;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
