bd6949cd8b7be6333ef22ba83bc2926aca8cb4b7
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-09-16 11:42:57 +0200

    feat: live update of metric

--- src/components/Data/GrafanaData.svelte
+++ src/components/Data/GrafanaData.svelte
@@ -7,8 +7,15 @@
   import { getMetricValue } from "@gapit/grafana-metric";
 
   let value: unknown = "No data";
+  let init = true;
 
-  dataStore.subscribe((_run) => {
+  $: if (alias) {
+    if (!init) updateValue();
+    // Don't trigger updateValue on init
+    init = false;
+  }
+
+  function updateValue() {
     if (!alias) return;
 
     const metricValue = getMetricValue(alias, { noDataValue: "No data" });
@@ -16,7 +23,9 @@
     if (typeof metricValue == "number")
       value = `${metricValue.toFixed(decimals)} ${unit}`;
     else value = "No data";
-  });
+  }
+
+  dataStore.subscribe(updateValue);
 </script>
 
 <tspan class="grafana-value">{value}</tspan>
