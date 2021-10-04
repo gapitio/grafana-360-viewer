4683655ae55ec5e03ac3465afcd6749870795a63
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-10-04 12:39:53 +0200

    chore: clean up unused code

--- src/components/HotspotContainer.svelte
+++ src/components/HotspotContainer.svelte
@@ -1,35 +1,10 @@
 <script lang="ts">
   import HotspotWrapper from "./HotspotWrapper.svelte";
   import type { HotspotConfig } from "../utils/getConfig";
-  import { currentSceneKeyStore } from "../stores";
   import type { SceneData } from "../utils/getSceneDataList";
 
   export let hotspotConfigList: HotspotConfig[];
   export let sceneDataList: SceneData[];
-
-  async function removePreviousHotspots() {
-    const scene = sceneDataList.find(
-      (scene) => scene.key == $currentSceneKeyStore
-    );
-    const hotspotCotainer = scene?.scene.hotspotContainer();
-    if (hotspotCotainer) {
-      for (const hotspot of hotspotCotainer.listHotspots()) {
-        hotspotCotainer.destroyHotspot(hotspot);
-      }
-    }
-  }
-
-  // $: if (sceneDataList) {
-  //   /*
-  //     Update hotspotConfig to make a new key based on the object
-  //     since objects are only equal to themself.
-  //    */
-  //   hotspotConfigList = hotspotConfigList.map((hotspotConfig) => ({
-  //     ...hotspotConfig,
-  //   }));
-
-  //   removePreviousHotspots();
-  // }
 </script>
 
 <div class="hotspot-container">
